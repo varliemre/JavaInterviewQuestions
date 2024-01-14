f59d64a88e95aa907d728a02d88d09b524dae469
Emre VARLI <varliemre02@gmail.com>
2024-01-14 10:29:07 +0100

    emre added

--- src/Study/Exercise1.java
+++ src/Study/Exercise1.java
@@ -4,7 +4,9 @@ public class Exercise1 {
     public static void main(String[] args) {
         System.out.println("14.01.2024 exercise line 1... ");
         System.out.println("14.01.2024 exercise line 2... ");
+        System.out.println("14.01.2024 exercise line 2,5 added... ");
         System.out.println("14.01.2024 exercise line 3... ");
+        System.out.println("14.01.2024 exercise line 3,5 added... ");
         System.out.println("14.01.2024 exercise line 4... ");
     }
 }
