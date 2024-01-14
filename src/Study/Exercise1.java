2432820bb53de05e04c0221d7b5bc64ae23329df
SukruYuksel <yksl3743dc@gmail.com>
2024-01-14 10:28:37 +0100

    sukru ekleme yapti

--- src/Study/Exercise1.java
+++ src/Study/Exercise1.java
@@ -4,6 +4,7 @@ public class Exercise1 {
     public static void main(String[] args) {
         System.out.println("14.01.2024 exercise line 1... ");
         System.out.println("14.01.2024 exercise line 2... ");
+        System.out.println("Sukru degisiklik yapti");
         System.out.println("14.01.2024 exercise line 3... ");
         System.out.println("14.01.2024 exercise line 4... ");
     }
