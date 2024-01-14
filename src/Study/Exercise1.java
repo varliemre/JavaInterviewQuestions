afeec11071a026f576d50be42c9597880b764330
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-14 09:20:07 +0000

    Main eklemeleri

---
+++ src/Study/Exercise1.java
@@ -0,0 +1,10 @@
+package Study;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("14.01.2024 exercise line 1... ");
+        System.out.println("14.01.2024 exercise line 2... ");
+        System.out.println("14.01.2024 exercise line 3... ");
+        System.out.println("14.01.2024 exercise line 4... ");
+    }
+}
