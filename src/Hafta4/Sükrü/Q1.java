2008ff4be6b30b6824017752d8dddb034a23b1d0
SukruYuksel <yksl3743dc@gmail.com>
2024-02-03 10:11:59 +0100

    4.Hafta sorular cözüldü

---
+++ src/Hafta4/Sükrü/Q1.java
@@ -0,0 +1,15 @@
+package Hafta4.Sükrü;
+
+import java.util.Map;
+
+public class Q1 {
+    public static void main(String[] args) {
+        String n = "19";
+
+        for (int i = 0; i < n.length(); i++) {
+
+        }
+    }
+
+
+
